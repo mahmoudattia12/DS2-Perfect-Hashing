3e8ec7c17dde39fa9994aa70a664436f64122fdd
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-05-17 19:42:35 +0300

    fixing a bug in Universe

--- src/UNIVERSAL/UniversalHashing.java
+++ src/UNIVERSAL/UniversalHashing.java
@@ -6,11 +6,11 @@ public class UniversalHashing {
     // h is a random b-by-u 0/1 matrix
     private int[][] h;
     private int hashTableLength;
+    private int u = 63; //number of bits for the max inserted key
 
     //int M = (int)Math.pow(2,Math.ceil(Math.log(n*n) / Math.log(2)));
     //or int M = (int)Math.pow(2,Math.ceil(Math.log(n) / Math.log(2)));
-    public UniversalHashing(int M, int maxKey){
-        int u = Integer.SIZE - Integer.numberOfLeadingZeros(maxKey); //number of bits for the max inserted key
+    public UniversalHashing(int M){
         int b = ( int)Math.ceil(Math.log(M) / Math.log(2));         //number of bits to represent h(x)
         h = new int[b][u];
         hashTableLength = M;
@@ -36,8 +36,8 @@ public class UniversalHashing {
     }
     private int[] getBinaryArray(int number) {
         int numBits = (int) (Math.log(number) / Math.log(2)) + 1;
-        int[] binaryArray = new int[numBits];
-        for (int i = numBits - 1; i >= 0; i--) {
+        int[] binaryArray = new int[Math.max(numBits, u)];
+        for (int i = Math.max(numBits, u) - 1; i >= 0; i--) {
             binaryArray[i] = number & 1;
             number = number >> 1;
         }
