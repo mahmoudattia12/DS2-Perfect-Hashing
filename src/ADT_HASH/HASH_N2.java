53e0d80249260ea4909d70c31b90cbbc62e95778
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2023-05-17 19:46:39 +0300

    fixing a bug in Universe

--- src/ADT_HASH/HASH_N2.java
+++ src/ADT_HASH/HASH_N2.java
@@ -2,44 +2,73 @@ package ADT_HASH;
 import UNIVERSAL.UniversalHashing;
 
 public class HASH_N2 {
-    int[] hashTable;
-    int n, maxKey;
+    public Integer[] hashTable;
+    public int n, rehashTries = 0;
     UniversalHashing h;
     public HASH_N2(){
         n = 0;
-        maxKey = 0;
     }
 
 
     public boolean insert(int key){
         n++;
-        maxKey = Math.max(key, maxKey);
-        int M = (int)Math.pow(2,Math.ceil(Math.log(n*n) / Math.log(2)));
-        hashTable = new int[M];
-        h = new UniversalHashing(M, maxKey);
-        boolean collision = false;
+        if(n == 1){
+            int M = (int)Math.pow(2,Math.ceil(Math.log(n*n) / Math.log(2)));
+            hashTable = new Integer[M];
+            h = new UniversalHashing(M);
+            int index = h.hash(key);
+            hashTable[index] = key;
+            return true;
+        }
 
-
-
-
-
-
-
-
+        int index = h.hash(key);
+        if(hashTable[index] != null && hashTable[index] != key){
+            rehashTries++;
+            return rehash(hashTable, key);
+        }else if(hashTable[index] != null && hashTable[index] == key){
+            n--;
+            return false;
+        }
+        hashTable[index] = key;
         return true;
     }
-    private void rehash(int[] prevTable){
 
+
+    private boolean rehash(Integer[] currTable, int newKey){
+        Integer[] prev = currTable;
+        boolean repeat = true;
+        boolean flag = false;
+        while (repeat){
+            repeat = false;
+            int M = (int)Math.pow(2,Math.ceil(Math.log(n*n) / Math.log(2)));
+            hashTable = new Integer[M];
+            h = new UniversalHashing(M);
+            for(int i = 0; i < prev.length; i++){
+                if(prev[i] != null){
+                    int index = h.hash(prev[i]);
+                    if(hashTable[index] != null&& hashTable[index] != prev[i]){
+                        repeat = true;
+                        rehashTries++;
+                        break;
+                    }
+                    hashTable[index] = prev[i];
+                }
+            }
+            if(!repeat){
+                int index = h.hash(newKey);
+                if(hashTable[index] != null && hashTable[index] != newKey){
+                    repeat = true;
+                    rehashTries++;
+                }else if(hashTable[index] != null && hashTable[index] == newKey){
+                    n--;
+                    flag =  false;
+                }else{
+                    hashTable[index] = newKey;
+                    flag = true;
+                }
+            }
+        }
+        return flag;
     }
-
-//    public void hashN2(){
-//        for(int i = 0; i < n; i++){
-//            h = new UniverseHashing(hashTable.length, maxKeyLength);
-////            int index = h.hash(toHash[i]);
-////            if(hashTable[index] == null){
-////
-////            }
-//        }
-//    }
 
 }
